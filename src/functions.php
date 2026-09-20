8a611c4a3cc0dc0c678d0bed184bd3adfcb3e6ee
Abel <absaal@alumnatflorida.es>
2026-09-20 18:28:52 +0200

    Cambios hechos en master

--- src/functions.php
+++ src/functions.php
@@ -4,7 +4,11 @@ function menssage(){
     echo "Hola ";
 }
 
+<<<<<<< HEAD
 function sum($num1, $num2){
+=======
+function sum($numero1, $numero2){
+>>>>>>> 20e0152 (Cambios hechos en master)
 
     $total= $num1 + $num2;
     return $total;
