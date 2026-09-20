fb221158515208e83704d418faedbff0e27f8489
Abel <absaal@alumnatflorida.es>
2026-09-20 19:36:14 +0200

    Rebase terminado en functions.php

--- src/functions.php
+++ src/functions.php
@@ -4,12 +4,8 @@ function menssage(){
     echo "Hola ";
 }
 
-<<<<<<< HEAD
-function sum($num1, $num2){
-=======
 function sum($numero1, $numero2){
->>>>>>> 20e0152 (Cambios hechos en master)
 
-    $total= $num1 + $num2;
+    $total= $numero1 + $numero2;
     return $total;
 }
