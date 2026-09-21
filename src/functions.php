90584d26df32654288c6efdcc7ff1085059e02c4
Abel <absaal@alumnatflorida.es>
2026-09-21 19:36:39 +0200

    Cambios hechos en rama funcionalidad-2, añadido nueva funcion

--- src/functions.php
+++ src/functions.php
@@ -9,3 +9,10 @@ function sum($numero1, $numero2){
     $total= $numero1 + $numero2;
     return $total;
 }
+
+//Cambios hechos dentro de rama funcionalidad-2
+function rest($numero1, $numero2){
+
+    $total= $numero1 - $numero2;
+    return $total;
+}
